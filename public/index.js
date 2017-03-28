240a1bc336f55e83f58152a8b3b409d1707c09fc
fchongg <fchongg@gmail.com>
2017-03-27 19:40:02 -0700

    added new href

--- public/index.js
+++ public/index.js
@@ -4,21 +4,21 @@ $(document).ready(function() {
     $("#name").hide();
     $("#searchResult").hide();
 
-    $("#signup").submit(function(){
-        var $form = $( this ),
-        name = $form.find( "input[id='username']" ).val()
-        $.ajax({
-          method: "POST",
-          url: "/api/users",
-          data: { name: name, utid: 1 }
-        })
-        .done(function( result ) {
-            user = result.data.name;
-            $("#welcome").hide();
-            $("#home").show();
-            $("#greeting").append(" " + user);
-        })
-    });
+    // $("#signup").submit(function(){
+    //     var $form = $( this ),
+    //     name = $form.find( "input[id='username']" ).val()
+    //     $.ajax({
+    //       method: "POST",
+    //       url: "/api/users",
+    //       data: { name: name, utid: 1 }
+    //     })
+    //     .done(function( result ) {
+    //         user = result.data.name;
+    //         $("#welcome").hide();
+    //         $("#home").show();
+    //         $("#greeting").append(" " + user);
+    //     })
+    // });
 
     $("#login").submit(function(){
         var $form = $( this ),
